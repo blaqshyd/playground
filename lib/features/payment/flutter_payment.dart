625ba2f50613f9e342d5c6a81276a4a23ff71875
Daniel A <dahblaqshyd@gmail.com>
2024-11-10 19:05:06 +0100

    🔥 Chore: Remove flutter_paypal_checkout

--- lib/features/payment/flutter_payment.dart
+++ lib/features/payment/flutter_payment.dart
@@ -1,9 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_paypal_checkout/flutter_paypal_checkout.dart';
-
-import '../../core/env.dart';
 
 class PaypalPayment extends StatefulWidget {
   const PaypalPayment({super.key});
@@ -13,70 +8,56 @@ class PaypalPayment extends StatefulWidget {
 }
 
 class _PaypalPaymentState extends State<PaypalPayment> {
-  void _paymentMethod() {
-    Navigator.of(context).push(MaterialPageRoute(
-      builder: (BuildContext context) => PaypalCheckout(
-        sandboxMode: true,
-        clientId: Env.clientId,
-        secretKey: Env.secretKey,
-        returnURL: "https://sandbox.paypal.com",
-        cancelURL: "https://sandbox.paypal.com",
-        transactions: const [
-          {
-            "amount": {
-              "total": '70',
-              "currency": "USD",
-              "details": {
-                "subtotal": '70',
-                "shipping": '0',
-                "shipping_discount": 0
-              }
-            },
-            "description": "The payment transaction description.",
-            "item_list": {
-              "items": [
-                {
-                  "name": "Apple",
-                  "quantity": 4,
-                  "price": '5',
-                  "currency": "USD"
-                },
-                {
-                  "name": "Pineapple",
-                  "quantity": 5,
-                  "price": '10',
-                  "currency": "USD"
-                }
-              ],
-              "shipping_address": {
-                "recipient_name": "Raman Singh",
-                "line1": "Delhi",
-                "line2": "",
-                "city": "Delhi",
-                "country_code": "IN",
-                "postal_code": "11001",
-                "phone": "+00000000",
-                "state": "Texas"
-              },
-            }
-          }
-        ],
-        note: "PAYMENT_NOTE",
-        onSuccess: (Map params) async {
-          log("onSuccess: $params");
-        },
-        onError: (error) {
-          log("onError: $error");
-          Navigator.pop(context);
-        },
-        onCancel: () {
-          log('cancelled:');
-        },
-      ),
-    ));
+//   void _paymentMethod() {
+//     Navigator.of(context).push(MaterialPageRoute(
+//       builder: (BuildContext context) => PaypalCheckout(
+//         sandboxMode: true,
+//         clientId: Env.clientId,
+//         secretKey: Env.secretKey,
+//         returnURL: "https://sandbox.paypal.com",
+//         cancelURL: "https://sandbox.paypal.com",
+//         transactions: const [
+//           {
+//             "amount": {
+//               "total": '70',
+//               "currency": "USD",
+//               "details": {"subtotal": '70', "shipping": '0', "shipping_discount": 0}
+//             },
+//             "description": "The payment transaction description.",
+//             "item_list": {
+//               "items": [
+//                 {"name": "Apple", "quantity": 4, "price": '5', "currency": "USD"},
+//                 {"name": "Pineapple", "quantity": 5, "price": '10', "currency": "USD"}
+//               ],
+//               "shipping_address": {
+//                 "recipient_name": "Raman Singh",
+//                 "line1": "Delhi",
+//                 "line2": "",
+//                 "city": "Delhi",
+//                 "country_code": "IN",
+//                 "postal_code": "11001",
+//                 "phone": "+00000000",
+//                 "state": "Texas"
+//               },
+//             }
+//           }
+//         ],
+//         note: "PAYMENT_NOTE",
+//         onSuccess: (Map params) async {
+//           log("onSuccess: $params");
+//         },
+//         onError: (error) {
+//           log("onError: $error");
+//           Navigator.pop(context);
+//         },
+//         onCancel: () {
+//           log('cancelled:');
+//         },
+//       ),
+//     ));
 
-    setState(() {});
-  }
+//     setState(() {});
+//   }
 
   @override
   Widget build(BuildContext context) {
@@ -92,7 +73,8 @@ class _PaypalPaymentState extends State<PaypalPayment> {
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _paymentMethod,
+        // onPressed: _paymentMethod,
+        onPressed: null,
         tooltip: 'Increment',
         label: const Text('Checkout'),
         icon: const Icon(Icons.add),
